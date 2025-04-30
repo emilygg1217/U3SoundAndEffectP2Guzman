ee9300a5a2a12dc5679ce89d7499ce4d3dccf023
emilygg1217 <emilyg1217@icloud.com>
2025-04-30 09:55:46 -0700

    s
    sx

--- P2SoundAndEffectGuzman/Assets/Scripts/PlayerController.cs
+++ P2SoundAndEffectGuzman/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce;
+    public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +29,17 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void OnCollisonEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+                gameOver = true;
+        }
     }
 }
